1a54eaf28f76763fdd3042dfb066ded783cd936a
Nicklaus <nicklaus.meadows@ufl.edu>
2025-02-12 21:57:07 -0500

    good start, data is flowing, but code is messy, and console log is close but something is slightly off still.

--- index.js
+++ index.js
@@ -11,3 +11,35 @@ var words = [
   'ukulele',
   'mango'
 ]
+
+var guessEl = document.getElementById('word-to-guess')
+var previousWordEl = document.getElementById('previous-word')
+var incorrectLettersEl = document.getElementById('incorrect-letters')
+var remainingGuessesEl = document.getElementById('remaining-guesses')
+var winsEl = document.getElementById('wins')
+var lossesEl = document.getElementById('losses')
+
+var wins = 0
+var losses = 0
+
+document.onkeyup = function (e) {
+  var key = e.key.toLowerCase()
+  if (/^[a-zA-Z]$/.test(key))// should filter non letters from my vague understanding of it.
+    console.log(e.key) // it does work on my console
+  // we need to randomly pick a word from the array like in the practice example
+  var randomWord = words[Math.floor(Math.random() * words.length)];
+  console.log(key, randomWord); // tests checks out 
+  // compare typed vs word options
+  if (key === randomWord) {
+    wins++
+    winsEl.textContent = wins
+  } else {
+    losses++
+    lossesEl.textContent = losses
+  }
+  previousWordEl.textContent = words
+
+  // calc scores
+  // display scores
+
+}
